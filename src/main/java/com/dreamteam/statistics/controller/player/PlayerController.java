ec9f0360ec7721bde467dafa9208ebd104a98c7d
ManoarySarobidy <manoarysarobidy@gmail.com>
2023-11-21 17:55:37 +0300

    edition

--- src/main/java/com/dreamteam/statistics/controller/player/PlayerController.java
+++ src/main/java/com/dreamteam/statistics/controller/player/PlayerController.java
@@ -7,17 +7,19 @@ import org.springframework.beans.factory.annotation.*;
 import com.dreamteam.statistics.model.player.Player;
 import com.dreamteam.statistics.response.Response;
 import com.dreamteam.statistics.repository.*;
-
+import java.util.*;
 @RestController
 @RequestMapping("/api/player")
 public class PlayerController{
 
+	@Autowired PlayerRepository pl;
 
 	@GetMapping
 	public Response getPlayers() throws Exception{
-		Player[] player = Player.getPlayersTable();
-		System.out.println("taille == " + player.length);
-		return new Response().addData("players" , player);
+		// Player[] player = Player.getPlayersTable();
+		List<Player> players = pl.findAll();
+		// System.out.println("taille == " + player.length);
+		return new Response().addData("players" , players);
 	}
 
 	@PostMapping
